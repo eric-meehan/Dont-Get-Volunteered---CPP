9e6035e7baaef91c6d2a9a3ce7f4fa833c1e8d97
Eric Meehan <eric.meehan@icloud.com>
2020-11-18 12:12:15 -0500

    Created the ChessBoard class

---
+++ Dont Get Volunteered - CPP/ChessBoard.cpp
@@ -0,0 +1,7 @@
+/*
+ Eric Meehan
+ 2020-11-18
+ 
+ Class implementation of a chess board that can be of any size
+*/
+
---
+++ Dont Get Volunteered - CPP/ChessBoard.hpp
@@ -0,0 +1,30 @@
+/*
+ Eric Meehan
+ 2020-11-18
+ 
+ Class representation of a chess board that can be of any size
+*/
+
+#ifndef ChessBoard_hpp
+#define ChessBoard_hpp
+
+#include <iostream>
+
+class ChessBoard
+{
+private:
+    int Length;
+    int Width;
+    int **Board;
+    // Mutators
+    void BoardBuilder();
+public:
+    // Constructors
+    ChessBoard();
+    ChessBoard(int, int);
+    // Accessors
+    int NumericPosition(int, int);
+    void CoordinatePosition(int*, int);
+};
+
+#endif /* ChessBoard_hpp */
